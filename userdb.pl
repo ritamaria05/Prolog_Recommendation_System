df8cae6869377142008aacf71c42df298f94f741
MaxSa2004 <max.phillips.sa@gmail.com>
2025-04-16 12:04:32 +0100

    .

--- userdb.pl
+++ userdb.pl
@@ -3,7 +3,7 @@ assert(db2('Orlando',userName,'Orlando')).
 assert(db2('Orlando',password,orlando)).
 assert(db2('Orlando',film,tt0004972)).
 assert(db2('Orlando',film,tt0012349)).
-%retractall(db2(_,film,tt0012349),1).
+retractall(db2(_,film,tt0012349),1).
 assert(db2('Max',userName,'Max')).
 assert(db2('Max',password,max)).
 assert(db2('Test1',userName,'Test1')).
